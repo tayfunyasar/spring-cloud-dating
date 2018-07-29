5ffd8c3979cebaaee22202d5ef809ff665cdb059
Tayfun Yaşar <tayfun.yasar@eteration.com>
2018-07-29 11:31:49 +0300

    #edge init

---
+++ proxy-server/src/main/java/com/tayfunyasar/dating/ProxyServerApplication.java
@@ -0,0 +1,12 @@
+package com.tayfunyasar.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServerApplication.class, args);
+	}
+}
